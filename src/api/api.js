792d60caa44d8d6555cb3940062d2a6f7267c41b
Andersonspita <andersonspita@hotmail.com.br>
2025-06-26 21:09:32 -0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const API_URL = 'https://gestao-condominio-api.onrender.com/api';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
-
 const api = axios.create({
     baseURL: API_URL,
 });
